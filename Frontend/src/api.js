2b7a382a627b1de85c1ad3d18755e20c49d2031b
JuanC <juancuellar8@gmail.com>
2025-10-26 19:55:53 -0500

    3.5Poligon

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
-const API_URL =  'http://localhost:5000/api';
+const API_URL =  import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 /*
-import.meta.env.VITE_API_URL ||
+
 */
 export const fetchTodosLosHospitales = async () => {
     const url = `${API_URL}/hospitales/todos`;
